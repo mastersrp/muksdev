1fbb39a464e92a20ce3a14e4f0edd12895d31692
T3hGamerDK <tcg.thegamer@gmail.com>
2012-12-17 19:47:21 +0100

    Fix creation of posts and discussions.

--- app/controllers/forum.rb
+++ app/controllers/forum.rb
@@ -24,6 +24,8 @@ PadrApp.controllers :forum do
 				redirect url(:forum,:new)
 			end
 			@discussion = Discussion.create(:title => params[:title], :account => current_account, :views => 0)
+			@post[:discussion] = @discussion.id
+			@post.save!
 			@discussion.posts << Post.get( @post.id )
 			@discussion.save!
 			if !@discussion then
@@ -36,13 +38,31 @@ PadrApp.controllers :forum do
   	end
   end
 
-	delete :destroy, :with => :id do
+	post :update, :with => :id do
+		if current_account != nil then
+			@discussion = Discussion.get( params[:id] )
+		else
+			flash[:warning] = "You do not have permissions to this!"
+			redirect url(:forum,:view,:id => params[:id])
+		end
+	end
+
+	get :destroy, :with => :id do
 		if current_account != nil then
 			if current_account.role == "admin" then
 				@discussion = Discussion.get( params[:id] )
+				@discussion.posts.each do |post|
+					post.destroy
+				end
+				@discussion.destroy
+				redirect url(:forum,:index)
 			else
 				flash[:warning] = "You do not have permissions to do this!"
+				redirect url(:forum,:index)
 			end
+		else
+			flash[:warning] = "You do not have permissions to do this!"
+			redirect url(:forum,:index)
 		end
 	end
 end
@@ -52,8 +72,8 @@ PadrApp.controllers :posts do
   post :create, :with => :id do
     if ( current_account != nil ) then
 			@discussion = Discussion.get( params[:id] )
-			@post = Post.create(:body => params[:body], :created_at => DateTime.now, :account => current_account )
-			if !@post then
+			@post = Post.create(:body => params[:post][:body], :created_at => DateTime.now, :account => current_account, :discussion => @discussion )
+			if !@post.save! then
 				flash[:error] = "Could not create new post!"
 				redirect url(:forum,:view,:id => @discussion.id)
 			end
@@ -61,13 +81,22 @@ PadrApp.controllers :posts do
 			@discussion.save!
 			redirect url(:forum,:view,:id => @discussion.id)
 		else
+			flash[:warning] = "Warning: You need to be logged in to create new posts."
 			redirect url(:accounts,:new)
 		end
 	end  
 
-	delete :destroy, :with => :id do
+	get :destroy, :with => :id do
 		if current_account != nil then
-			@dpost = Post.get( params[:id] )
+			unless current_account.role == "admin" then
+				flash[:warning] = "You do not have permissions to do this!"
+			end
+			@post = Post.get( params[:id] )
+			@post.destroy
+			redirect url(:forum,:view, :id => @post.discussion)
+		else
+			flash[:warning] = "You do not have permission to do this!"
+			redirect url(:forum,:view, :id => @post.discussion)
 		end
 	end
 end
